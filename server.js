ff820b40d7e2e25798629505bfecf4754bf828d7
Jerry Zou <zry656565@gmail.com>
2015-10-20 22:17:36 +0800

    server done

---
+++ server.js
@@ -0,0 +1,29 @@
+/**
+ * @author: Jerry Zou
+ * @email: jerry.zry@outlook.com
+ */
+
+var http = require('http')
+  , exec = require('exec')
+
+var deployServer = http.createServer(function(request, response) {
+  if (request.url.search(/deploy$/i) > 0) {
+    exec(['git', 'pull'], function(err, out, code) {
+      if (err instanceof Error) {
+        response.writeHead(500)
+        response.end('Server Internal Error.')
+        throw err
+      }
+      process.stderr.write(err)
+      process.stdout.write(out)
+      response.writeHead(200)
+      response.end('Deploy Done.')
+    });
+
+  } else {
+    response.writeHead(404)
+    response.end('Not Found.')
+  }
+})
+
+deployServer.listen(9988)
